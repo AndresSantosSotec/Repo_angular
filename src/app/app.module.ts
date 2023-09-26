577986dedbd20a6f184c8f74d4a055f5e850b0e6
SpeedDevloperAndres <andressantostkd@gmail.com>
2023-09-25 19:08:23 -0600

    Primer commit de angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ListproductsComponent } from './components/listproducts/listproducts.component';
+import { AddEditProductsComponent } from './components/add-edit-products/add-edit-products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ListproductsComponent,
+    AddEditProductsComponent
   ],
   imports: [
     BrowserModule,
